be61282ece26c9b843a0afd61a0e12bbeeffac7a
paduazin <luiz.poubel@aluno.senai.br>
2025-08-26 17:17:24 -0300

    to aprendendo mano

---
+++ canvas/ex1.js
@@ -0,0 +1,37 @@
+var carro = {}
+console.log(carro);
+
+carro.marca = "Chevrolet"
+carro.modelo = "Corvette"
+carro.ano = 2025
+
+
+
+//EX2
+var carro = {}
+console.log(carro);
+
+carro.marca = "Chevrolet"
+carro.modelo = "Corvette"
+carro.ano = 2023
+
+console.log(carro);
+
+console.log(carro.marca);
+
+carro.ano = 2025
+carro.cor = "rosa"
+
+
+// EX3
+
+var pessoas = {
+  nome: "renan",
+  idade: "28",
+    apresentar: function(){
+        console.log("Olá, meu nome é", pessoas.nome, "e tenho", pessoas.idade, "anos");
+        
+    }
+}
+pessoas.apresentar()
+
